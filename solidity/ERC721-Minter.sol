b40d772c1b68f8ecc2b40cf745807f21494433de
h4shell <fw1987@gmail.com>
2022-07-21 19:53:56 +0200

    update erc721

--- solidity/ERC721-Minter.sol
+++ solidity/ERC721-Minter.sol
@@ -1,18 +1,40 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
- 
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
- 
-contract newNFT is NFTokenMetadata, Ownable {
- 
-  constructor() {
-    nftName = "iWantYourNFT";
-    nftSymbol = "IWYN";
-  }
- 
-  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
-    super._mint(_to, _tokenId);
-    super._setTokenUri(_tokenId, _uri);
-  }
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract HuskyArt is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("iWantYoutNFT", "IWYN") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
