4e99a87ce6c2fbdc42701b4854aa0115ca848ca7
iWantYourNFT <109594467+iWantYourNFT@users.noreply.github.com>
2023-02-08 11:55:40 +0100

    Update newERC721.sol

--- solidity/newERC721.sol
+++ solidity/newERC721.sol
@@ -1,43 +1,121 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+// SPDX-License-Identifier: MIT LICENSE
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract HuskyArt is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+pragma solidity ^0.8.0;
 
-    Counters.Counter private _tokenIdCounter;
+contract Collection is ERC721Enumerable, Ownable {
+
+    struct TokenInfo {
+        IERC20 paytoken;
+        uint256 costvalue;
+    }
+
+    TokenInfo[] public AllowedCrypto;
     
+    using Strings for uint256;
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public maxSupply = 1000;
+    uint256 public maxMintAmount = 5;
+    bool public paused = false;
 
-    constructor() ERC721("iWantYourNFT", "IWYN") {}
+    constructor() ERC721("Net2Dev NFT Collection", "N2D") {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://h4sh.it/NFT/";
+    function addCurrency(
+        IERC20 _paytoken,
+        uint256 _costvalue
+    ) public onlyOwner {
+        AllowedCrypto.push(
+            TokenInfo({
+                paytoken: _paytoken,
+                costvalue: _costvalue
+            })
+        );
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-      
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    function _baseURI() internal view virtual override returns (string memory) {
+    return "ipfs://EE5MmqVp5MmqVp7ZRMBBizicVh9ficVh9fjUofWicVh9f/";
+
     }
 
-    // The following functions are overrides required by Solidity.
+    function mint(address _to, uint256 _mintAmount, uint256 _pid) public payable {
+        TokenInfo storage tokens = AllowedCrypto[_pid];
+        IERC20 paytoken;
+        paytoken = tokens.paytoken;
+        uint256 cost;
+        cost = tokens.costvalue;
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= maxSupply);
+            
+            if (msg.sender != owner()) {
+            require(msg.value == cost * _mintAmount, "Not enough balance to complete transaction.");
+            }
+            
+            for (uint256 i = 1; i <= _mintAmount; i++) {
+                paytoken.transferFrom(msg.sender, address(this), cost);
+                _safeMint(_to, supply + i);
+            }
+        }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
+        function walletOfOwner(address _owner)
         public
         view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+        returns (uint256[] memory)
+        {
+            uint256 ownerTokenCount = balanceOf(_owner);
+            uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+            for (uint256 i; i < ownerTokenCount; i++) {
+                tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+            }
+            return tokenIds;
+        }
+    
+        
+        function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory) {
+            require(
+                _exists(tokenId),
+                "ERC721Metadata: URI query for nonexistent token"
+                );
+                
+                string memory currentBaseURI = _baseURI();
+                return
+                bytes(currentBaseURI).length > 0 
+                ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+                : "";
+        }
+        // only owner
+        
+        function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+            maxMintAmount = _newmaxMintAmount;
+        }
+        
+        function setBaseURI(string memory _newBaseURI) public onlyOwner() {
+            baseURI = _newBaseURI;
+        }
+        
+        function setBaseExtension(string memory _newBaseExtension) public onlyOwner() {
+            baseExtension = _newBaseExtension;
+        }
+        
+        function pause(bool _state) public onlyOwner() {
+            paused = _state;
+        }
+        
+        function withdraw(uint256 _pid) public payable onlyOwner() {
+            TokenInfo storage tokens = AllowedCrypto[_pid];
+            IERC20 paytoken;
+            paytoken = tokens.paytoken;
+            paytoken.transfer(msg.sender, paytoken.balanceOf(address(this)));
+        }
 }
